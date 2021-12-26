db27d415a87968adb41ca80f80a2ce662d123969
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2021-12-26 14:11:58 +0300

    add login authentication

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,14 @@
+const userModel = require('../models/userModel');
 
 const login_get =  (req,res) =>{
     res.render('login');
 }
 
-const login_post = (req,res)=>{
-    res.send('deneme');
+const login_post = async function(req,res){
+    const {email, password} = req.body;
+    const user = await userModel.loginUser(email,password);
+
+    //res.redirect('/votes');
 }
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,39 @@
+const bcrypt = require('bcrypt');
 
-
-const saveUser = (userObject) =>{
-
+//veti tabanı simülasyonu
+const userDbSimulation = {
+    username: 'ferhat',
+    email: 'ferhat@gmail.com',
+    password: '$2b$10$uD6wCKH5gOE5BxUH.bDeOe3QtC0KSDp8XO2qR24EhGeo/LJuckAOi'
 }
 
-module.exports = saveUser;
+//save user
+const saveUser = async function(userObject){
+    const salt = await bcrypt.genSalt();
+    userObject.password = await bcrypt.hash(userObject.password,salt);
+    console.log(userObject.password);
+    /*
+     * buraya veri tabanına user ekleme işlemleri gelecek
+     * 
+     */
+}
+
+
+//login
+const loginUser = async function (email,password){
+    if(email== userDbSimulation.email){
+        const auth = await bcrypt.compare(password,userDbSimulation.password);
+        if(auth){
+            return userDbSimulation;
+        }else{
+            throw Error('parola hatalı');
+        }
+    }else{
+        throw Error('Böyle bir kullanıcı yok');
+    }
+}
+
+module.exports = {
+    saveUser,
+    loginUser
+};
